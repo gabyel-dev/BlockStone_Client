55aac86180d4a56672179c5d0bc3da4cb0fe1604
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2026-03-26 00:58:39 +0800

    changed server

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://blockstone-server.onrender.com",
         changeOrigin: true,
       },
     },
