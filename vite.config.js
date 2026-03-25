04df24158e95d873fb568f2abf70f349e7f72ba9
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2026-03-26 01:27:12 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://blockstone-server.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  },
 });
