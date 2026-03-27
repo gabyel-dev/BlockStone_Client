3a5d780af26ea7a75ef227418ca302daa03b29f9
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2026-03-27 23:20:34 +0800

    UPDATE: added shift agenda

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
+  //for local testing
+  /*   server: {
     proxy: {
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
-  },
+  }, */
 });
