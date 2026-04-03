a4d3b64ae491116e51bb5fccab3cf0b064e69e5f
gabyel-dev <184721807+gabyel-dev@users.noreply.github.com>
2026-04-03 13:32:40 +0800

    test

--- vercel.js
+++ vercel.js
@@ -1,10 +1,8 @@
-const server = import.meta.env.VITE_API_BASE_URL;
-
 export const config = {
   rewrites: [
     {
       source: "/api/:path*",
-      destination: "https://blockstone-server.onrender.com",
+      destination: import.meta.env.VITE_API_BASE_URL,
     },
     {
       source: "/(.*)",
